8305d13278074299ea58e8fb3a1141466a79a652
Khalid Atlassi <katlassi_ext@coyote-group.com>
2024-01-22 11:07:15 +0100

    Adding methods to list/update for pgsql

--- src/PgsqlConnector.php
+++ src/PgsqlConnector.php
@@ -51,4 +51,70 @@ class PgsqlConnector implements DbConnectionInterface
         return $this->pdo->exec($query);
     }
 
+
+    /**
+     * Return rows
+     *
+     * @param $sql
+     * @param $params
+     * @return mixed
+     */
+    public function executeAndReturnRows($sql, $params = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
+    }
+
+    /**
+     * Return count
+     *
+     * @param $sql
+     * @param array $params
+     * @return mixed
+     */
+    public function executeAndReturnCount($sql, array $params = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->rowCount();
+    }
+    /**
+     * Return count
+     *
+     * @param $sql
+     * @param array $params
+     * @return mixed
+     */
+    public function executeAndReturnOne($sql, array $params = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($params);
+        $resultArray = $stmt->fetchAll();
+        return $resultArray?$resultArray[0]:$resultArray;
+    }
+
+    /**
+     * Execute insert or update sql
+     *
+     * @param $sql
+     * @param array $params
+     * @return bool
+     */
+    function executeInsertOrUpdate($sql, array $params = []) {
+        $stmt = $this->pdo->prepare($sql);
+
+        foreach ($params as $param => $value) {
+            $paramType = PDO::PARAM_STR;
+
+            if (is_int($value)) {
+                $paramType = PDO::PARAM_INT;
+            } elseif (is_bool($value)) {
+                $paramType = PDO::PARAM_BOOL;
+            } elseif (is_null($value)) {
+                $paramType = PDO::PARAM_NULL;
+            }
+
+            $stmt->bindValue($param, $value, $paramType);
+        }
+
+        return $stmt->execute();
+    }
 }
